4deab127c981c31e4dba705406834e16355939e8
ithSokny <ithSokny@gmail.com>
2024-01-24 16:46:50 +0700

    new_file

---
+++ index.js
@@ -0,0 +1,13 @@
+var admins = [{ username: "admin", password: "110503" }];
+$(document).ready(()=>{
+    $(".btn-login").click(() => {
+        let username = $("#username").val();
+        let password = $("#password").val();
+        let valid = admins.some(e => e.username == username && e.password == password);
+        if (valid) {
+            location.href = "./First_Project/home.html";
+        } else {
+            alert("Incorrect Username and password!");
+        }
+    });
+})
